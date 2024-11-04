c1c0f7938f18f905ad33c5ccefdac7aac5e540dc
Rishi-Mittal-daffodil <rishi.mittal@daffodilsw.com>
2024-11-04 11:44:42 +0530

    cors origin added

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(morgan("common"));
 const allowedOrigins = process.env.CORS_ORIGIN.split(',');
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://trendify1.netlify.app',
         credentials: true,
     })
 );
